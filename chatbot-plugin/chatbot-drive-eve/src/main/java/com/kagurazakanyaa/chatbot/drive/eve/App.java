51be125b2cf07ef1d5000c48ba7eea6d126e9af1
KagurazakaNyaa <i@kagurazakanyaa.com>
2020-08-18 23:21:19 +0800

    添加插件项目

---
+++ chatbot-plugin/chatbot-drive-eve/src/main/java/com/kagurazakanyaa/chatbot/drive/eve/App.java
@@ -0,0 +1,13 @@
+package com.kagurazakanyaa.chatbot.drive.eve;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ chatbot-plugin/chatbot-drive-mirai/src/main/java/com/kagurazakanyaa/chatbot/drive/mirai/App.java
@@ -0,0 +1,13 @@
+package com.kagurazakanyaa.chatbot.drive.mirai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
