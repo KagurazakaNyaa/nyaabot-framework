1f399c9e57dd5533703d47d69c684757086f4caf
KagurazakaNyaa <i@kagurazakanyaa.com>
2020-08-19 00:12:21 +0800

    添加插件项目

---
+++ chatbot-plugin/chatbot-drive-discord/src/main/java/com/kagurazakanyaa/chatbot/drive/discord/App.java
@@ -0,0 +1,13 @@
+package com.kagurazakanyaa.chatbot.drive.discord;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
