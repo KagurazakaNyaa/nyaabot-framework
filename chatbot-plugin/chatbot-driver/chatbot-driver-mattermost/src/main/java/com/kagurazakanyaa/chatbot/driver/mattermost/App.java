f324744df79be6d0b35fcaabc9aabb6f032dea05
神楽坂·喵 <i@kagurazakanyaa.com>
2020-08-19 15:11:22 +0800

    迁移包位置

---
+++ chatbot-plugin/chatbot-driver/chatbot-driver-mattermost/src/main/java/com/kagurazakanyaa/chatbot/driver/mattermost/App.java
@@ -0,0 +1,13 @@
+package com.kagurazakanyaa.chatbot.driver.mattermost;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
